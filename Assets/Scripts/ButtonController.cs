d0b6fb13ce1ee528b6345c40f62bb30f9d823476
Anoop <b.anoop13@gmail.com>
2024-08-07 17:48:02 -0700

    Implemented Spawning of Objects
    There is a UI that can be used to scroll through some of the optical objects and spawn them into the scene, through the use of buttons.

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField] 
+    private SpawnOpticalScrollScript spawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (gameObject.name == "SpawnButton")
+        {
+            spawn.SpawnPress();
+        }
+        else if (gameObject.name == "RightScrollButton")
+        {
+            
+            spawn.RightPress();
+        }
+        else if (gameObject.name == "LeftScrollButton")
+        {
+            spawn.LeftPress();
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnOpticalScrollScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class SpawnOpticalScrollScript : MonoBehaviour
+{
+    [SerializeField] RectTransform rectTransform;
+    [SerializeField] Transform spawnPosition;
+    [SerializeField] List<GameObject> opticalObject;
+    
+
+    int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void RightPress()
+    {
+        if(index < (opticalObject.Count - 1))
+        {
+            index++;
+            rectTransform.localPosition += new Vector3(-600, 0);
+        }
+        else
+        {
+            index = 0;
+            rectTransform.localPosition += new Vector3(600 * (opticalObject.Count - 1), 0);
+        }
+    }
+
+    public void LeftPress() 
+    {
+        if(index > 0)
+        {
+            index--;
+            rectTransform.localPosition += new Vector3(600, 0);
+        }
+        else
+        {
+            index = (opticalObject.Count - 1);
+            rectTransform.localPosition += new Vector3(-600 * (opticalObject.Count - 1), 0);
+        }
+        
+    }
+
+    public void SpawnPress()
+    {
+        GameObject spawnThisObject = opticalObject[index];
+
+        Instantiate(spawnThisObject, spawnPosition.position, spawnPosition.rotation);
+    }
+}
