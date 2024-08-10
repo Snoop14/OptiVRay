0611bd9c9d64673ff9f981ab8567c1a8e5ac6c71
Anoop <b.anoop13@gmail.com>
2024-08-09 21:42:23 -0700

    Added buttons for filter spawn
    Added some buttons to be used to control what color of filter lens is spawned.

---
+++ Assets/Scripts/ButtonFiltersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFiltersController : MonoBehaviour
+{
+    [SerializeField]
+    private SpawnFilterScript spawnFilt;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(gameObject.name == "redButton")
+        {
+            spawnFilt.AddRemoveColour(0);
+        }
+        else if(gameObject.name == "greenButton")
+        {
+            spawnFilt.AddRemoveColour(1);
+        }
+        else if (gameObject.name == "blueButton")
+        {
+            spawnFilt.AddRemoveColour(2);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnFilterScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFilterScript : MonoBehaviour
+{
+    private Color filtColor;
+    private Renderer filtRenderer;
+    private Vector3 rotSpeed;
+
+    [SerializeField]
+    private LineRenderer beam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filtColor = Color.black;
+        filtColor.a = 175f / 255f;
+        filtRenderer = GetComponent<Renderer>();
+        filtRenderer.material.color = filtColor;
+        rotSpeed = new Vector3(0, 50, 0);
+    }
+    private void Update()
+    {
+        transform.Rotate(rotSpeed * Time.deltaTime);
+    }
+
+    public void AddRemoveColour(int colIndex)
+    {
+        if (filtColor[colIndex] == 1f)
+        {
+            filtColor[colIndex] = 0f;
+        }
+        else
+        {
+            filtColor[colIndex] = 1f;
+        }
+
+        if (filtColor.r == 1f && filtColor.g == 1f && filtColor.b == 1f)
+        {
+            filtColor.a = 125f / 255f;
+        }
+        else
+        {
+            filtColor.a = 175f / 255f;
+        }
+
+        filtRenderer.material.color = filtColor;
+        beam.endColor = filtColor;
+
+    }
+
+    public void spawnFilter()
+    {
+
+    }
+}
